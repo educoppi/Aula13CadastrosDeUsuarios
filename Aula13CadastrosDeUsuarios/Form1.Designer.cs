c7035525790facb70e79c666c7c93ce3eb24de58
eduardo.bcoppi <eduardo.bcoppi@CAR0588296W10-1>
2023-07-26 17:15:55 -0300

    Adicionar arquivos de projeto.

---
+++ Aula13CadastrosDeUsuarios/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Aula13CadastrosDeUsuarios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(109, 93);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(369, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(56, 93);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Idade:";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(109, 125);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(29, 20);
+            this.txtIdade.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Rua:";
+            // 
+            // txtRua
+            // 
+            this.txtRua.Location = new System.Drawing.Point(109, 164);
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(237, 20);
+            this.txtRua.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cidade:";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(109, 199);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(174, 20);
+            this.txtCidade.TabIndex = 6;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(109, 291);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(207, 38);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "SALVAR";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(514, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Pessoas Cadastradas:";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(517, 63);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(246, 321);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 404);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRua);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRua;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Aula13CadastrosDeUsuarios/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula13CadastrosDeUsuarios
+{
+    public partial class Form1 : Form
+    {
+
+        void limpaCampos()
+        {
+            txtNome.Text = null;
+            txtIdade.Text = null;
+            txtRua.Text = null;
+            txtCidade.Text = null;
+        }
+
+        public void AtualizaInterface()
+        {
+            listView1.Clear();
+
+            for (int i = 0; i < pessoas.Count(); i++)
+            {
+                listView1.Items.Add(pessoas[i].nome);
+            }
+        }
+
+        public bool camposPreenchidos(TextBox nome, TextBox idade, TextBox rua, TextBox cidade)
+        {
+
+            if (nome.Text == "" || idade.Text == "" || rua.Text == "" || cidade.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        List<Pessoa> pessoas = new List<Pessoa>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (camposPreenchidos(txtNome, txtIdade, txtRua, txtCidade) == false)
+            {
+                MessageBox.Show("Preencha todos os campos!!!");
+                limpaCampos();
+                return;
+            }
+
+            string nome = txtNome.Text;
+            int idade = int.Parse(txtIdade.Text);
+            string rua = txtRua.Text;
+            string cidade = txtCidade.Text;
+
+            Pessoa pessoa = new Pessoa(nome, idade, rua, cidade);
+            pessoas.Add(pessoa);
+
+            limpaCampos();
+            AtualizaInterface();
+        }
+    }
+}
